799a6a22e168550889066007644ef0ec958c4445
Jiho Choi <withj1123@gmail.com>
2023-11-09 09:35:31 +0900

    version update

--- lib/const/agora.dart
+++ lib/const/agora.dart
@@ -1,5 +1,5 @@
-const APP_ID = '앱ID를 입력해주세요!!!';
+const APP_ID = '7cb15bd951224c328fe8ecf89703ba65';
 
-const CHANNEL_NAME = '채널이름을 입력해주세요!!!';
+const CHANNEL_NAME = 'testchannel';
 
-const TEMP_TOKEN = '토큰값을 입력해주세요!!!';
+const TEMP_TOKEN = '0067cb15bd951224c328fe8ecf89703ba65IABrpi/EdysMSlpRuTXhkJNCgSwzQsym42lryzV71rfh/epuE8wAAAAAEAC8e3Fntl/WYgEAAQC2X9Zi';
